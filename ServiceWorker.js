f37a7b93487f4de7e798e08c7d62de00c0dd73c5
Oleksii Arefiev <oleksii.arefiev.work@gmail.com>
2025-03-14 15:00:38 +0200

    Test build

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-My project-0.1";
 const contentToCache = [
     "Build/Build.loader.js",
-    "Build/Build.framework.js.gz",
-    "Build/Build.data.gz",
-    "Build/Build.wasm.gz",
+    "Build/Build.framework.js.unityweb",
+    "Build/Build.data.unityweb",
+    "Build/Build.wasm.unityweb",
     "TemplateData/style.css"
 
 ];
